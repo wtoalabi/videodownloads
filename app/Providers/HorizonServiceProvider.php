18651c7dae2508fcb73813021395c18007229f9d
Wale Alabigir congitgit config --global user.emaail wtoalabi@gmail.comgit configgit config --global user.emaile wtoalabi@gmail.com <wtoalabi@gmail.com>
2021-01-13 16:49:54 +0100

    horizon auth

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -1,42 +1,56 @@
 <?php
-
-namespace App\Providers;
-
-use Illuminate\Support\Facades\Gate;
-use Laravel\Horizon\Horizon;
-use Laravel\Horizon\HorizonApplicationServiceProvider;
-
-class HorizonServiceProvider extends HorizonApplicationServiceProvider
-{
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot() {
-        parent::boot();
-
-        // Horizon::routeSmsNotificationsTo('15556667777');
-        // Horizon::routeMailNotificationsTo('example@example.com');
-        // Horizon::routeSlackNotificationsTo('slack-webhook-url', '#channel');
-
-        // Horizon::night();
-    }
-
-    /**
-     * Register the Horizon gate.
-     *
-     * This gate determines who can access Horizon in non-local environments.
-     *
-     * @return void
-     */
-    protected function gate()
+    
+    namespace App\Providers;
+    
+    use Illuminate\Support\Facades\Gate;
+    use Laravel\Horizon\Horizon;
+    use Laravel\Horizon\HorizonApplicationServiceProvider;
+    use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+    
+    class HorizonServiceProvider extends HorizonApplicationServiceProvider
     {
-        Gate::define('viewHorizon', function ($user) {
-            return in_array($user->email, [
-                'wtoalabi@gmail.com',
-                'u@u.com'
-            ]);
-        });
+        /**
+         * Bootstrap any application services.
+         *
+         * @return void
+         */
+        public function boot() {
+            parent::boot();
+            Horizon::auth(function ($request) {
+                if ($request->ajax()) {
+                    return true;
+                } else if (env('APP_ENV') === 'local') {
+                    return true;
+                } else if(isset($request->let_me_go) && $request->let_me_go == 'ok'){
+                    return true;
+                }
+                else {
+                    throw new UnauthorizedHttpException('Unauthorized');
+                }
+            });
+            
+            
+            // Horizon::routeSmsNotificationsTo('15556667777');
+            // Horizon::routeMailNotificationsTo('example@example.com');
+            // Horizon::routeSlackNotificationsTo('slack-webhook-url', '#channel');
+            
+            // Horizon::night();
+        }
+        
+        /**
+         * Register the Horizon gate.
+         *
+         * This gate determines who can access Horizon in non-local environments.
+         *
+         * @return void
+         */
+        protected function gate() {
+            Gate::define('viewHorizon', function ($user) {
+                return in_array($user->email, [
+                    'wtoalabi@gmail.com',
+                    'u@u.com'
+                ]);
+            });
+            
+        }
     }
-}
